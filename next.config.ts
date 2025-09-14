fcd237012d43d1edeff5fb9564665ba94b7a0080
Aldo Almeida <aldo@alchemax.com.br>
2025-09-13 22:19:35 -0300

    api on

--- next.config.ts
+++ next.config.ts
@@ -6,14 +6,10 @@ const nextConfig: NextConfig = {
     unoptimized: true
   },
   async rewrites() {
-    const apiUrl = process.env.API_URL;
-    if (!apiUrl) {
-      throw new Error('API_URL environment variable is not defined. Please set it in your .env.local file.');
-    }
     return [
       {
         source: '/api/:path*',
-        destination: `${apiUrl}/:path*`,
+        destination: `${process.env.API_URL}/:path*`,
       },
     ]
   },
